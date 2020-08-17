4db5dbfe768234795928f576312904243b0ae879
Vladimir <vladimir.dolgov@yahoo.com>
2020-08-17 22:11:28 +0300

    implemented pagination into api

--- mainapp/api/api_views.py
+++ mainapp/api/api_views.py
@@ -1,13 +1,33 @@
+from collections import OrderedDict
+
+from rest_framework.response import Response
 from rest_framework.generics import ListAPIView, RetrieveAPIView
 from rest_framework.filters import SearchFilter
+from rest_framework.pagination import PageNumberPagination
 
 from .serializers import CategorySerializer, SmartphoneSerializer, NotebookSerializer
 from ..models import Category, Smartphone, Notebook
 
 
+class CategoryPagination(PageNumberPagination):
+
+    page_size = 2
+    page_query_param = 'page_size'
+    max_page_size = 10
+
+    def get_paginated_response(self, data):
+        return Response(OrderedDict([
+            ('objects_count', self.page.paginator.count),
+            ('next', self.get_next_link()),
+            ('previous', self.get_previous_link()),
+            ('items', data),
+        ]))
+
+
 class CategoryListAPIView(ListAPIView):
 
     serializer_class = CategorySerializer
+    pagination_class = CategoryPagination
     queryset = Category.objects.all()
 
 
