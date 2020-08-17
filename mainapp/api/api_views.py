160cbc6536b8a431c2dd04dda930e30b756aa99e
Vladimir <vladimir.dolgov@yahoo.com>
2020-08-17 21:55:39 +0300

    added notebook api and detail (retreive) apis

--- mainapp/api/api_views.py
+++ mainapp/api/api_views.py
@@ -1,4 +1,4 @@
-from rest_framework.generics import ListAPIView
+from rest_framework.generics import ListAPIView, RetrieveAPIView
 from rest_framework.filters import SearchFilter
 
 from .serializers import CategorySerializer, SmartphoneSerializer, NotebookSerializer
@@ -25,3 +25,17 @@ class NotebookListAPIView(ListAPIView):
     queryset = Notebook.objects.all()
     filter_backends = [SearchFilter]
     search_fields = ['price', 'title']
+
+
+class SmartphoneDetailAPIView(RetrieveAPIView):
+
+    serializer_class = SmartphoneSerializer
+    queryset = Smartphone.objects.all()
+    lookup_field = 'id'
+
+
+class NotebookDetailAPIView(RetrieveAPIView):
+
+    serializer_class = NotebookSerializer
+    queryset = Notebook.objects.all()
+    lookup_field = 'id'
--- mainapp/api/urls.py
+++ mainapp/api/urls.py
@@ -1,10 +1,18 @@
 from django.urls import path
 
-from .api_views import CategoryListAPIView, SmartphoneListAPIView, NotebookListAPIView
+from .api_views import (
+    CategoryListAPIView,
+    SmartphoneListAPIView,
+    NotebookListAPIView,
+    SmartphoneDetailAPIView,
+    NotebookDetailAPIView,
+)
 
 
 urlpatterns = [
-    path('categories/', CategoryListAPIView.as_view(), name='categories'),
-    path('smartphones/', SmartphoneListAPIView.as_view(), name='smartphones'),
-    path('notebooks/', NotebookListAPIView.as_view(), name='notebooks'),
+    path('categories/', CategoryListAPIView.as_view(), name='categories_list'),
+    path('smartphones/', SmartphoneListAPIView.as_view(), name='smartphones_list'),
+    path('notebooks/', NotebookListAPIView.as_view(), name='notebooks_list'),
+    path('smartphones/<str:id>/', SmartphoneDetailAPIView.as_view(), name='smartphone_detail'),
+    path('notebooks/<str:id>/', NotebookDetailAPIView.as_view(), name='notebook_detail'),
 ]
